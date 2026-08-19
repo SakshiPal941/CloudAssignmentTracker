3944fe10386f59873bb489c087fd54a5dd038713
Sakshi Pal <sakshipal55@gmail.com>
2026-08-19 15:43:17 +1200

    fixed assignmentservice bug

--- cloudcomputing/CloudAssignmentTracker/Backend/src/main/java/com/example/assignment_tracker/AssignmentController.java
+++ cloudcomputing/CloudAssignmentTracker/Backend/src/main/java/com/example/assignment_tracker/AssignmentController.java
@@ -2,6 +2,7 @@ package com.example.assignment_tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 import java.util.Arrays;
@@ -9,9 +10,12 @@ import java.util.Arrays;
 @RestController
 @CrossOrigin(origins = "http://127.0.0.1:5500")
 public class AssignmentController {
+@Autowired
+//private AssignmentService AssignmentService;
 
     @GetMapping("/api/assignments")
     public List<String> getAssignments() {
         return Arrays.asList("Cloud Project Part 1", "Database Lab 3", "System Design Essay");
+       // return AssignmentService.getAllAssignments();
     }
 }
